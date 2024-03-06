800c4f209303d7d344695d80abff0bc69c96f57e
Parampreet Singh <rubercuber.30@gmail.com>
2024-03-06 18:34:23 +0530

    Added Translator

---
+++ utils/translator.py
@@ -0,0 +1,8 @@
+from deep_translator import GoogleTranslator
+
+def translate(text, from_lang, to_lang):
+    return GoogleTranslator(source=from_lang, target=to_lang).translate(text)
+
+if __name__ == "__main__":
+    sentence = "My name is Parampreet Singh. I am doing Bachelors of Science in Data Science and Applications from IIT Madras, India."
+    print(translate(sentence, "en", "hi"))
